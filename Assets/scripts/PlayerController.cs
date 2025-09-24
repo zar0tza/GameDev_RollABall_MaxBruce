bcf8d107ca3126411edb128fb01741d8c50fbf90
Max <maxbruce04@hotmail.com>
2025-09-24 10:54:57 -0300

    collectables and enemy

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
-        private float movementX;
-        private float movementY;
+    private int count;
+    private float movementX;
+    private float movementY;
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
     
     void OnMove(InputValue movementValue)
@@ -22,10 +30,32 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText() 
+   {
+       countText.text =  "Count: " + count.ToString();
+
+       if (count >= 12)
+       {
+           winTextObject.SetActive(true);
+       }
+   }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+       {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+       }
+       
     }
 }
