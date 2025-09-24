246d5d4a9a35ee76d3e0000ac9fbb9be70caba4e
Max <maxbruce04@hotmail.com>
2025-09-24 08:42:21 -0300

    changed speed for player

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+
     private Rigidbody rb;
         private float movementX;
         private float movementY;
@@ -24,6 +26,6 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
-        rb.AddForce(movement);
+        rb.AddForce(movement * speed);
     }
 }
